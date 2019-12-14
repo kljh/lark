cfcb83d7e91836559cf1acdaf7e7c38996ae962d
Claude Cochet <code@main2.fr>
2019-12-14 12:33:06 +0000

    VBA parser and transpiler to Python.
    >T

--- examples/vba_demo_input.bas
+++ examples/vba_demo_input.bas
@@ -1,10 +1,62 @@
+' Extended Excel context menu
+' Author : Claude Cochet
+' Created : 2010-10-06
+' !! Error if you add space after comment
+Const a = 3
+
 Option Explicit
 Option Base 5.3
 
+' Demo VBA file for Python transpiler
 
 Sub fct()
+	' Static Variable
+    Static bRefreshCalcTimeDisplay As Boolean
+
+	Goto anchor
+anchor:
+	Dim i%, j%, nl%, nh%, ml%, mh%
+	Dim i&, j&, nl&, nh&, ml&, mh& ' flt#
+	ReDim header$(ml% To mh%)
+
 	Dim a
+	Dim b As Long
+	Dim shp As New Shape
+
+	Dim ctlg
+	Set ctlg = New ADOX.Catalog
+	Dim ctlg2 : Set ctlg2 = New ADOX.Catalog
+	'!! Dim cnx As New ADODB.Connection
+
 	a = 3
-	then
     b = 3
+
+	' Reminder : SHIFT is +, CTRL is ^ (caret), ALT is %, ALPHA ENTER is ~ or {RETURN}, NUMERIC KEYPAD ENTER is {ENTER}
+	' Other keys: {UP}, {DOWN}, {LEFT}, {RIGHT}, {PGUP}, {PGDN}, {DOWN}, {ESC},  {DEL}, {CLEAR}, {BS}, {BREAK}, {HOME}, {INS}, {TAB}, {F1},.. {F15}
+	Application.OnKey "{F1}", "SendKeysF2"          ' F1 is useless, time wasting
+	' a comment after an inline comment
+	Application.OnKey "^!", "ToggleTwoDigits"       ' extend standard shortcut
+
+	'!! Error if no parentheses below
+	'!! Call tmp.Sort
+	Call fct()
+	Call fct( 123, 456 )
+	Call fct( a := 123, b := 456 )
+	Call fct( 123, New Dictionary )
+	Call fct( 123, 456, )
+	Call fct( 123,, 789 )
+	Call fct( , 456, 789 )
+	Call fct( 123, New Dictionary )
+	names_dict.Add nm_sheet, New Dictionary
+
+	Err.Raise vbObjectError, , "Oops"
+
+	'!! Error below
+	'sht.cells(1, 1).Activate
+	'names_dict.Item(nm_sheet).Add nm_bare, nm.RefersTo ' nm.RefersTo or nm.RefersToR1C1
+
+	Dim FileNumber: FileNumber = FreeFile
+    Open export_path For Output Access Write As #FileNumber
+    Print #FileNumber, json
+    Close #FileNumber
 End Sub
--- examples/vba_parser.py
+++ examples/vba_parser.py
@@ -10,58 +10,89 @@ from lark import Lark, Transformer, v_args
 
 
 grammar = r"""
-    ?start: statement*
+    ?start: NEWLINE* statements
+
+    !statements: ( ( ( statement | statements_inline ) smnt_new_line ) | ( COMMENT_EOL NEWLINE* ) | ( goto_anchor_stmt NEWLINE*  ) )*
+    !statements_inline: statement ( ":" statement )+
+
+    smnt_new_line: COMMENT_EOL NEWLINE* | NEWLINE+
 
     ?statement: ( option_stmt
+        | preprocessor_if_stmt
+        | function_declare_stmt
         | function_definition_stmt
-        | statement_expr ) ( ":" | NEWLINE )+
-
-    ?statement_expr: ( on_error_stmt
+        | on_error_stmt
+        | goto_stmt
         | dim_stmt
         | sub_call_stmt
         | assign_stmt
         | if_stmt
         | for_stmt
-        | loop_stmt )
+        | loop_stmt
+        | with_stmt
+        | open_stmt
+        | close_stmt
+        | print_stmt )
 
     !option_stmt: "Option"i ( "Explicit"i | "Base"i NUMBER )
 
     ?on_error_stmt: "On"i "Error"i ( "GoTo"i  (NAME|"0") | "Resume"i next )
+    !goto_stmt: "GoTo"i  VB_NAME
+    !goto_anchor_stmt: VB_NAME GOTO_ANCHOR_EOL
 
-    ?dim_stmt: ( "ReDim"i | "Dim"i | ( dim_stmt "," ) ) NAME [ "(" ranges ")" ] [ as NAME ]
+    !dim_stmt: ( "Static" | private | "ReDim"i | "Dim"i | ( dim_stmt "," ) ) VB_NAME [ "(" ranges ")" ] [ as [ "New"i ] type_name ]
     ?ranges: range | ranges "," range
     ?range: expr | ( expr to expr )
 
-    ?function_definition_stmt.1: ( "Sub" | "Function" ) NAME "(" function_args ")" ( ":" | NEWLINE )  statement* "End" ( "Sub" | "Function" )
-    ?function_args: function_args "," function_arg | function_arg |
-    function_arg: [ "Optional" ] ["ByRef"] NAME [ as NAME ] [ "=" expr ]
+    function_declare_stmt: ("Public"i|"Private"i)? "Declare"i "PtrSafe"i? ( "Sub"i | "Function"i ) NAME "Lib"i VB_STRING "(" function_args ")"  [ as NAME ]
 
-    !assign_stmt: [ set ] lvalue "=" expr
-    ?lvalue: ( name | func_call )
+    ?function_definition_stmt.2: ("Public"i|"Private"i)? ( "Sub"i | "Function"i ) NAME "(" function_args ")" [ as NAME ] smnt_new_line  statements end ( "Sub" | "Function" )
+    ?function_args: function_args "," function_arg | function_arg |
+    function_arg: [ "Optional" ] ["ByRef"|"ByVal"] NAME [ as NAME ] [ "=" expr ]
+
+    // ("Const"i|"Public"i|"Private"i)?
+    !assign_stmt: [ set ] lvalue "="  expr
+    ?lvalue: "."? ( name | func_call )
     // !! func_call is lvalue because we can have my2darray(1, 2) = "abc"
 
     ?expr: name | VB_STRING | NUMBER | func_call | expr_binary | expr_other
-    ?expr_binary: expr ( "=" | "<>" | ">" | "<" | ">=" | "<=" | "+" | "-" | "*" | "/" | "&" | "And"i | "AndAlso"i | "Or"i | "OrElse"i | "Xor"i ) expr
-    ?expr_other: ( "-" expr ) | ( "Not" expr ) | ( "(" expr ")" )
+    ?expr_binary: expr ( "=" | "<>" | ">" | "<" | ">=" | "<=" | "Is"i | "+" | "-" | "*" | "/" | "&" | "." | "And"i | "AndAlso"i | "Or"i | "OrElse"i | "Xor"i ) expr
+    ?expr_other: ( "-" expr ) | ( "." expr ) | ( "Not" expr ) | ( "(" expr ")" ) | ( "New"i expr )
 
-    ?name: ( VB_NAME  "." )*  VB_NAME
+    !type_name: NAME
+    ?name: (NAME|VB_NAME)  ( "." (NAME|VB_NAME) )*
 
-    !sub_call_stmt: name func_args
-        | call name "(" func_args ")"
+    !sub_call_stmt: "."? name func_args
+        | call name ( "(" func_args ")" ) ?
 
     !func_call: name "(" func_args ")"
-    !func_args: func_args "," [ NAME ":=" ] func_arg | [ NAME ":=" ] func_arg |
-    !func_arg: expr
+    !func_args: func_args "," func_arg | func_args "," | "," func_args | func_arg |
+    !func_arg: [ VB_NAME ":=" ] expr
 
-    ?if_stmt: if expr then statement_expr [ else statement_expr]
-        | if expr then NEWLINE+ statement+ ( elseif expr then NEWLINE+ statement+ )* [ else NEWLINE+ statement+] end if
+    ?if_stmt: if expr then statement [ else statement ]
+        | if expr then smnt_new_line statements ( elseif expr then smnt_new_line statements )* [ else smnt_new_line statements ] end if
 
-    ?for_stmt: for each VB_NAME in expr NEWLINE+ statement+ next [VB_NAME]
-        | for VB_NAME "=" expr to expr [ step expr ] NEWLINE+ statement+ next [VB_NAME]
+    !preprocessor_if_stmt: "#If" expr then smnt_new_line statements ( "#" elseif expr then smnt_new_line statements )* [ "#Else" smnt_new_line statements ] "#End If"
 
-    ?loop_stmt: "While"i expr NEWLINE+ statement+ "Wend"i
-        | "Do"i  "While"i expr NEWLINE+ statement+ "Loop"i
-        | "Do"i  "Until"i expr NEWLINE+ statement+ "Loop"i
+    ?for_stmt: for each VB_NAME in expr smnt_new_line statements next [VB_NAME]
+        | for VB_NAME "=" expr to expr [ step expr ] smnt_new_line statements next [VB_NAME]
+
+    ?loop_stmt: "While"i expr smnt_new_line statements "Wend"i
+        | "Do"i  "While"i expr smnt_new_line statements "Loop"i
+        | "Do"i  "Until"i expr smnt_new_line statements "Loop"i
+        | "Do"i  smnt_new_line statements "Loop"i "While"i expr
+
+    ?with_stmt: with expr smnt_new_line statements end with
+    !with_prefix: "."?
+
+    open_stmt: "Open"i expr [open_mode_clause] [open_access_clause] [open_lock_clause] "As"i file_number [open_len_clause]
+    open_mode_clause : for (  "Append" | "Binary" | "Input" | "Output" | "Random" )
+    open_access_clause : "Access" ( "Read" | "Write" | ("Read" "Write") )
+    open_lock_clause :  "Shared" |  ( "Lock" "Read" ) | ("Lock" "Write") |  ("Lock" "Read" "Write")
+    open_len_clause : "Len" "=" expr
+    close_stmt: "Close"i file_number
+    print_stmt: "Print"i file_number "," func_args
+    file_number : "#" expr
 
     ?sum: product
         | sum "+" product   -> add
@@ -76,14 +107,16 @@ grammar = r"""
          | name             -> var
          | "(" sum ")"
 
-    ?set : "Set"
-    ?as : "AS"| "as" | "As"
-    ?call: "CALL" | "call" | "Call"
-    ?if : "If"
-    ?elseif : "ElseIf"
-    ?then : "Then"
-    ?else : "Else"
-    ?end : "End"
+    ?private: "Private"i
+    ?set : "Set"i
+    ?as : "As"i
+    ?call: "Call"i
+
+    ?if : "If"i
+    ?elseif : "ElseIf"i
+    ?then : "Then"i
+    ?else : "Else"i
+    ?end : "End"i
 
     ?for: "For"i
     ?to: "To"i
@@ -92,76 +125,88 @@ grammar = r"""
     ?in : "In"i
     ?next : "Next"i
 
+    ?with : "With"i
+
+    GOTO_ANCHOR_EOL: /:\r*\n/
+
+
     CONTINUE_LINE: /_\r*\n/
     %ignore CONTINUE_LINE
 
-    COMMENT:  "'" /[^\n]*/ "\n"
+    // PREPROCESSOR_LINE: /#[^\n]*(\r?\n\r?)+/
+
+    COMMENT_EOL : /'[^\n]*\n/
+
+    //COMMENT:  /'[^\n]*\n/
     VB_STRING : "\"" /([^"]|"")*/ "\""
-    %ignore COMMENT
+    //%ignore COMMENT
 
     // %import common.CNAME -> NAME
     %import common.LETTER
     %import common.DIGIT
     NAME: LETTER ("_"|LETTER|DIGIT)*
 
-    %import common.ESCAPED_STRING
     %import common.NUMBER
-    %import common.WS_INLINE
     %import common.NEWLINE
+    %import common.WS_INLINE
     %ignore WS_INLINE
 
-    // $ String, % Integer, F& Long, # Double, ! Single, @ Currency
+    // $ String, % Integer, & Long, # Double, ! Single, @ Currency
     VB_NAME: NAME /[$%&#!@]?/
 
 """
 
 vbparser = Lark(grammar, parser='lalr')  # 'lalr' or 'earley'
 
-def generate_all(src_root):
+def transpile_all(src_root):
     import os
 
     filepaths = []
     for dirpath, dirnames, filenames in os.walk(src_root):
         filepaths = filepaths + [ os.path.join(dirpath, f) for f in filenames if f.endswith(".bas") ]
-    print("#files", len(filepaths))
+    print("#files", len(filepaths), "\n")
 
-    for f in filepaths:
-        generate_one(f)
+    for i, f in enumerate(filepaths):
+        print(f"{i+1}/{len(filepaths)}")
+        try:
+            transpile_one(f, verbose=False)
+        except Exception as e:
+            print(">!", str(e).split('\n')[0])
+        print()
 
-def generate_one(src_path):
-    parse_one(src_path)
+def transpile_one(src_path, verbose=False):
+    import vba_py_generator as vba2py
 
-def parse_one(src_path):
+    ast = parse_one(src_path, verbose)
+
+    #src = vba2py.generate(ast)
+    #with open(src_path+".py", "w") as f:
+    #    f.write(src)
+
+def parse_one(src_path, verbose=False):
     with open(src_path, "r") as f:
         lines = f.readlines()
-        lines = lines[1:]
+        lines = lines[1:] # !!
         txt = "".join(lines)
 
-    print("src_path", src_path)
+    print("src_path", src_path, "\t\t", "#lines", len(lines))
 
     try:
         ast = vbparser.parse(txt)
-        print(ast.pretty())
+        return ast
     except Exception as e:
-        traceback.print_exc()
-
-        tmp = "".join([ f"{i+1}  {line}" for i, line in enumerate(lines) ])
-        print(tmp)
-        #raise e
-
-def test():
-    print(calc("a = 1+2"))
-    print(calc("1+a*-3"))
-
-
+        if verbose:
+            traceback.print_exc()
+            tmp = "".join([ f"{i+1}  {line}" for i, line in enumerate(lines) ])
+            print(tmp)
+        raise e
 
 if __name__ == '__main__':
-    # test()
 
     src_root =r"..\..\xjs\vba"
-    generate_all(src_root)
+    transpile_all(src_root)
 
-    generate_one(r"test.bas")
-    #generate_one(src_root + r"\modWalkDir.bas")
-    #generate_one(src_root + r"\modHttpRequest.bas")
-
+    transpile_one(r"vba_demo_input.bas")
+    #transpile_one(src_root + r"\modWalkDir.bas")
+    #transpile_one(src_root + r"\modHttpRequest.bas")
+    transpile_one(src_root + r"\modXtendedXl.bas")
